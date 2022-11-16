bbe59393050ca7b83cc0ce42b337355d1733da7e
Andrei <abirul@mail.ru>
2022-11-16 23:35:05 +0300

    Commit #4

--- js/script.js
+++ js/script.js
@@ -30,4 +30,53 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+  //Timer
+  const dedline = "16-11-22";
+
+  function getTimeremaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((t / 1000 / 60) % 60);
+    const seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+
+    function getZero(num) {
+      if (num >= 0 && num < 10) {
+        return `0{num}`;
+      } else {
+        return num;
+      }
+    }
+  }
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector("#days");
+    const hours = timer.querySelector("#hours");
+    const minutes = timer.querySelector("#minutes");
+    const seconds = timer.querySelector("#seconds");
+    const timeInterval = setInterval(upDateClock, 1000);
+
+    upDateClock();
+
+    function upDateClock() {
+      const t = getTimeremaining(endtime);
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock(".timer", dedline);
 });
