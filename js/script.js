6ad3963a2ae288217e96e2a280c539195b249036
Andrei <abirul@mail.ru>
2022-12-08 23:23:26 +0300

    Commit #2

--- js/script.js
+++ js/script.js
@@ -430,4 +430,70 @@ window.addEventListener("DOMContentLoaded", function () {
       dots[slideIndex - 1].style.opacity = 1;
     });
   });
+
+  // Calculator
+
+  const result = document.querySelector(".calculating__result span");
+  let sex, height, weight, age, ratio;
+
+  function calcTotal() {
+    if (!sex || !height || !weight || !age || !ratio) {
+      result.textContent = "___";
+      return;
+    }
+
+    if (sex === "female") {
+      result.textContent =
+        (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio;
+    } else {
+      result.textContent =
+        (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio;
+    }
+  }
+  calcTotal();
+
+  function getStaticInformation(parrentSelector, activeClass) {
+    const element = document.querySelectorAll(`${parrentSelector} div`);
+    document.querySelector(parrentSelector).addEventListener("click", (e) => {
+      if (e.target.getAttribute("data-ratio")) {
+        ratio = +e.target.getAttribute("data-ratio");
+      } else {
+        sex = e.target.getAttribute("id");
+      }
+
+      element.forEach((elem) => {
+        elem.classList.remove(activeClass);
+      });
+
+      e.target.classList.add(activeClass);
+
+      calcTotal();
+    });
+  }
+  getStaticInformation("#gender", "calculating__choose-item_active");
+  getStaticInformation(
+    ".calculating__choose_big",
+    "calculating__choose-item_active"
+  );
+
+  function getDynamicInformation(selector) {
+    const input = document.querySelector(selector);
+    input.addEventListener("input", () => {
+      switch (input.getAttribute("id")) {
+        case "hieght":
+          height = +input.value;
+          break;
+        case "weight":
+          weight = +input.value;
+          break;
+        case "age":
+          age = +input.value;
+          break;
+      }
+    });
+    calcTotal();
+  }
+  getDynamicInformation("#hieght");
+  getDynamicInformation("#weight");
+  getDynamicInformation("#age");
 });
