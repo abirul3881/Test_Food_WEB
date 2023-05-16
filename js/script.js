ab69b80f12bf4c91c91177034151707845d5cd48
Andrei <abirul@mail.ru>
2023-05-16 22:52:33 +0300

    Commit #1

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 window.addEventListener("DOMContentLoaded", () => {
   /*Далее пишем код*/
 
-  /*Работаем с табами на странице*/
+  /*Работа с табами на странице*/
   const tabs = document.querySelectorAll(".tabheader__item");
   const tabsContent = document.querySelectorAll(".tabcontent");
   const tabsParent = document.querySelector(".tabheader__items");
@@ -17,10 +17,24 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   }
   /*Функция которая показывает табы*/
-  function showTabsPage(i) {
+  function showTabsPage(i = 0) {
     tabsContent[i].style.display = "block";
     tabs[i].classList.add("tabheader__item_active");
   }
   hideTabsPage();
-  showTabsPage(0);
+  showTabsPage();
+
+  /*Смена табов и обработчик событий*/
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((elem, num) => {
+        if (target == elem) {
+          hideTabsPage();
+          showTabsPage(num);
+        }
+      });
+    }
+  });
 });
