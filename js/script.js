cff82222a5180ed84fd325c2d9e32385cea9a31d
Andrei <abirul@mail.ru>
2023-05-19 22:50:11 +0300

    Commit #4

--- js/script.js
+++ js/script.js
@@ -38,16 +38,31 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  /* =============================================================================== */
+
   /* Таймер (счётчик)*/
 
-  const dedLain = "2023-05-17"; /*отправная точка таймера*/
+  const dedLain = "2025-05-17"; /*отправная точка таймера*/
 
   function getTimeRemaining(endtime) {
+    let days;
+    let hours;
+    let minutes;
+    let seconds;
+
     const t = Date.parse(endtime) - Date.parse(new Date());
-    const days = Math.floor(t / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    const minutes = Math.floor((t / 1000 / 60) % 60);
-    const seconds = Math.floor((t / 1000) % 60);
+
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+      minutes = Math.floor((t / 1000 / 60) % 60);
+      seconds = Math.floor((t / 1000) % 60);
+    }
 
     return {
       total: t,
@@ -57,6 +72,42 @@ window.addEventListener("DOMContentLoaded", () => {
       seconds: seconds,
     };
   }
+  /* Функция добавления ноля в часы, минуты, секунды */
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
   /* Функция установки часов на страницу */
-  
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector("#days");
+    const hours = timer.querySelector("#hours");
+    const minutes = timer.querySelector("#minutes");
+    const seconds = timer.querySelector("#seconds");
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    /* Функция обновления таймера */
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      /* Остановка таймера */
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock(".timer", dedLain);
 });
